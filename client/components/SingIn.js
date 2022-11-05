7c36b07866f663553f277e3d0b321e13526d7a5b
eyuel gebreselassie <eyuelmulugeta05@gmail.com>
2022-11-05 04:13:09 -0400

    added a sign up form

--- client/components/SingIn.js
+++ client/components/SingIn.js
@@ -8,8 +8,14 @@ export default function SingIn() {
       <div className="bg-gradient-to-r from-red-600 via-indigo-600 to-blue-600 h-content rounded-[25px] flex justify-between">
         <div>
           {" "}
-          <form>
-            <h1>Get Started</h1> <input />
+          <form className="flex items-center flex-col gap-[3rem] p-[5rem]">
+            <h1 className="text-7xl">Get Started</h1>
+            <input placeholder="Name" />
+            <input placeholder="Email" />
+            <input placeholder="Password" />
+            <button className="text-black rounded-md w-[15rem] py-[.8rem] bg-gradient-to-r from-blue-600 via-indigo-600 to-red-600">
+              Sign In
+            </button>
           </form>
         </div>
         <div className="h-content max-h-[100%]">
